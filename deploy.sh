d9cc7ee1863a967aa04426409d76a5fccaaa339a
winhows5 <wangwh258@gmail.com>
2023-01-07 21:56:20 -0500

    Add deploy script for servers

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# For CentOS 7+. 
+
+cd
+
+yum -y install tmux nodejs
+git clone https://github.com/winhows5/google_play_scraper.git
+
+cd google_play_scraper/
+npm install csv-parser google-play-scraper
+
+tmux
