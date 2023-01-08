26cec8546c312f7890b55b968c097258f24d769f
winhows5 <wangwh258@gmail.com>
2023-01-07 22:00:30 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # For CentOS 7+. 
 
-cd
+# git clone https://github.com/winhows5/google_play_scraper.git
 
+cd
 yum -y install tmux nodejs
-git clone https://github.com/winhows5/google_play_scraper.git
 
 cd google_play_scraper/
 npm install csv-parser google-play-scraper
